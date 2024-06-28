ca0b976f689bcdf3900b1501095e76942110b676
María Ríos <145855086+claudiamariariosespinosa@users.noreply.github.com>
2024-06-27 23:35:08 -0500

    Update colegio.java
    codigo java

--- src/colegio.java
+++ src/colegio.java
@@ -5,11 +5,7 @@ public class edades_estudiantes {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         float[] numero = new float[10];
-        double media_estudiantes=0;
-        double prom_estudiantes = 0;
-        double conteo_cincos=0;
-        double alumnos = 0, edad = 0, edad2 = 0;
-
+        double media_estudiantes=0,  prom_estudiantes = 0, conteo_cincos=0, alumnos = 0, edad = 0, edad2 = 0;
         System.out.printf("bienvenido al programa de el colegio");
         System.out.printf("¿cuales son las edades de cuantos alumnos desea digitar?");
         alumnos = entrada.nextDouble();
@@ -22,21 +18,24 @@ public class edades_estudiantes {
                 edad = edad + edad2;
                 edad2 = edad;
                 prom_estudiantes = edad / alumnos;}
-                System.out.printf("el promedio de la edad de los alumnos es " + prom_estudiantes);
+            System.out.printf("el promedio de la edad de los alumnos es " + prom_estudiantes);
             if (edad<5){
                 media_estudiantes=edad/i;}
-                System.out.printf("la media de los estudiantes es "+media_estudiantes);
+            System.out.printf("la media de los estudiantes es "+media_estudiantes);
 
-           if (edad==5) {
-               conteo_cincos++;}
-               System.out.printf("el numero de 5 es " + conteo_cincos);
-
-
-            }
+            if (edad==5) {
+                conteo_cincos++;}
+            System.out.printf("el numero de 5 es " + conteo_cincos);
 
 
         }
+
+
     }
+}
+
+
+
 
 
 
